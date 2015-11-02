540f35a4503a750d9ce0f80bb7ee90dd5dfddd45
ynvygy <lugerand2004@yahoo.com>
2015-11-02 21:42:52 +0200

    Added searching and sorting

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,12 @@
 class ProductsController < ApplicationController
+	helper_method :sort_column, :sort_direction
 	before_action :set_product, only:[:show]
 	def index
 		@category = Category.find(params[:category_id])
 		if params[:search]
 			@products = @category.products.search(params[:search]).paginate(:per_page=>10, :page=>params[:page])
 		else
-			@products = @category.products.paginate(:per_page=>10, :page=>params[:page])
+			@products = @category.products.paginate(:per_page=>10, :page=>params[:page]).order(sort_column + " " + sort_direction)
 		end
 	end
 	
@@ -17,4 +18,11 @@ class ProductsController < ApplicationController
     def set_product
       @product = Product.find(params[:id])
     end
+    def sort_column
+    	Product.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  	end
+  
+  	def sort_direction
+    	%w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  	end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
 	def index 
 		@categories = Category.all
 		@products = Product.all
+		@category = Category.all
 	end
 end
